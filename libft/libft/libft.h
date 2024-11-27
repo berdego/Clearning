65603b49491f3c6504893fe475287cd8f6b40a59
Tomatte <daniel.brandao2000@gmail.com>
2024-11-26 21:29:58 -0300

    add libft

---
+++ libft/libft/libft.h
@@ -0,0 +1,7 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+void hello(void);
+void oi(void);
+
+#endif
---
+++ libft/myprogram.c
@@ -0,0 +1,8 @@
+#include "./mylib/libft.h"
+
+int main(void)
+{
+    hello();
+    oi();
+    return (0);
+}
